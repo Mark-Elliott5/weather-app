4ccf69a7df6f99fa4b8362d4839574440b05e6fe
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-07-18 14:50:35 -0400

    Location, date, and time now updates

--- src/apiCaller.js
+++ src/apiCaller.js
@@ -11,8 +11,9 @@ const apiCaller = (() => {
       const data = await call.json();
       console.log(data);
       domManipulator.updatePage(data);
-    } catch (err) {
-      alert(err);
+    } catch (error) {
+      console.warn(error);
+      domManipulator.stopLoadingIcon();
     }
   };
 
@@ -49,71 +50,3 @@ const apiCaller = (() => {
 })();
 
 export default apiCaller;
-
-// async function test() {
-//     try {
-//         throw new Error('oops');
-//     } catch (error) {
-//             console.log('error');
-//             return Promise.reject(new Error(error));
-//         }
-//     }
-
-// async function test2() {
-//   try {
-//       const test3 = await test();
-//   } catch (error) {
-//       console.log('error caught')
-//   }
-// }
-
-// // Check if geolocation is supported by the browser
-// if ("geolocation" in navigator) {
-//   // Prompt user for permission to access their location
-//   navigator.geolocation.getCurrentPosition(
-//     // Success callback function
-//     (position) => {
-//       // Get the user's latitude and longitude coordinates
-//       const lat = position.coords.latitude;
-//       const lng = position.coords.longitude;
-
-//       // Do something with the location data, e.g. display on a map
-//       console.log(`Latitude: ${lat}, longitude: ${lng}`);
-//     },
-//     // Error callback function
-//     (error) => {
-//       // Handle errors, e.g. user denied location sharing permissions
-//       console.error("Error getting user location:", error);
-//     }
-//   );
-// } else {
-//   // Geolocation is not supported by the browser
-//   console.error("Geolocation is not supported by this browser.");
-// }
-// const initialQuery = async () => {
-//   try {
-//     const currentLocation = await getCurrentLocation();
-//     const data = await searchQuery(currentLocation);
-//     return data;
-//   } catch (error) {
-//     let message;
-//     if (error === 1) {
-//       message = 'User denied location services. ';
-//     } else if (error === 2) {
-//       message = 'Position unavailable. ';
-//     } else {
-//       message = 'Location services request timed out. ';
-//     }
-//     alert(`${message}Please search for a location instead.`);
-//     return null;
-//   }
-// };
-// const getCurrentLocation = async () => {
-//   const coordinates = navigator.geolocation.getCurrentPosition((position) => {
-//     const { latitude } = position.coords;
-//     const { longitude } = position.coords;
-//     const location = `${latitude},${longitude}`;
-//     return location;
-//   });
-//   return coordinates;
-// };
--- src/domManipulator.js
+++ src/domManipulator.js
@@ -9,19 +9,55 @@ const domManipulator = (() => {
     loading.classList.add('hidden');
   };
 
+  const convertTime = (input) => {
+    const dateOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    const timeOptions = {
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+    const [date, time] = [new Date(input), new Date(input)];
+    const formattedDate = date.toLocaleString('en-US', dateOptions);
+    const formattedTime = time.toLocaleString('en-US', timeOptions);
+    return [formattedDate, formattedTime];
+  };
+
   const updatePage = (data) => {
-    const todayMinTemp = data.forecast.forecastday[0].day.mintemp_f;
-    const todayMaxTemp = data.forecast.forecastday[0].day.maxtemp_f;
-    const tomorrowMinTemp = data.forecast.forecastday[1].day.mintemp_f;
-    const tomorrowMaxTemp = data.forecast.forecastday[1].day.maxtemp_f;
-    const thirdDayMinTemp = data.forecast.forecastday[2].day.mintemp_f;
-    const thirdDayMaxTemp = data.forecast.forecastday[2].day.maxtemp_f;
+    const { forecast } = data;
+    const todayMinTemp = forecast.forecastday[0].day.mintemp_f;
+    const todayMaxTemp = forecast.forecastday[0].day.maxtemp_f;
+    const tomorrowMinTemp = forecast.forecastday[1].day.mintemp_f;
+    const tomorrowMaxTemp = forecast.forecastday[1].day.maxtemp_f;
+    const thirdDayMinTemp = forecast.forecastday[2].day.mintemp_f;
+    const thirdDayMaxTemp = forecast.forecastday[2].day.maxtemp_f;
     const todayTemps = document.getElementById('forecast-today-temps');
     const tomorrowTemps = document.getElementById('forecast-tomorrow-temps');
     const thirdDayTemps = document.getElementById('forecast-dat-temps');
     todayTemps.textContent = `${todayMinTemp} - ${todayMaxTemp}`;
     tomorrowTemps.textContent = `${tomorrowMinTemp} - ${tomorrowMaxTemp}`;
     thirdDayTemps.textContent = `${thirdDayMinTemp} - ${thirdDayMaxTemp}`;
+
+    const { location } = data;
+    const { name } = location;
+    const [currentDate, currentTime] = convertTime(location.localtime);
+    const locationElem = document.getElementById('location');
+    locationElem.textContent = name;
+    const dateElem = document.getElementById('date');
+    dateElem.textContent = currentDate;
+    const timeElem = document.getElementById('time');
+    timeElem.textContent = currentTime;
+
+    // const todaysWeather = data.current;
+    // const currentTemp = todaysWeather.temp_f;
+    // const feelsLike = todaysWeather.feelslike_f;
+    // const { humidity } = todaysWeather;
+    // const condition = todaysWeather.condition.text;
+    // const sunrisesunset = `${forecast.forecastday[0].astro.sunrise} - ${forecast.forecastday[0].astro.sunset}`;
+    // const wind = `${todaysWeather.wind_mph} ${wind_dir}`;
+    // const currentTemp = ;
     stopLoadingIcon();
   };
 
